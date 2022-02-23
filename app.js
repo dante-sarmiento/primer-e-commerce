8b7d544013e5da437f8c7b33090e8c82a4c69d8e
dante-sarmiento <dante.sarmientof5@gmail.com>
2022-02-23 13:58:42 -0300

    metodos GET/PUT/DELETE

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ var userController = require('./controllers/user.controller');
 app.use(express.json());//funcion para poder leer los valores enviados en metodos post mediante el body
 app.use(express.urlencoded({extended: true}));
 
-app.get('/api/user', userController.getUsers);
+app.get('/api/users', userController.getUsers);
+
 app.post('/api/user', userController.addUser);
+//app.delete('api/user')
+
+
+app.get('/api/user', userController.getUser);
+
+app.delete('api/user', userController.deleteUser);
+
 
 module.exports = app;
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,7 +6,7 @@ function addUser(req, res){
     // newUser.fullName = req.body.fullName;
     // newUser.email = req.body.email;
     // newUser.password = req.body.password;
-    newUser.save()
+    newUser.save()// sirve para guardar en la base de datos
     res.send({ usuarioNuevo: newUser })
 }
 
@@ -15,7 +15,31 @@ async function getUsers(req, res){
     res.send({ users: usuariosDB })
 }
 
+async function getUser(req, res){
+    //id que recibimos desde el endpoint
+    const userId = req.query.user_id;
+    //buscamos especificamente ese id en nuestra colleccion users(base de datos)
+    const user = await User.findById(userId);
+    console.log(user)
+    // si no encontramos el usuario
+    if(!user) return res.status(404).send ('no se encontro el usuario que busca');
+
+    return res.status(200).send({ user: user })
+}
+
+async function deleteUser(req, res){
+    const userId = req.query.user_id;
+
+    const user = await User.findByIdAndDelete(userId);
+    console.log(user);
+    
+    res.send({ userDeleted: userId });
+}
+
+
 module.exports = {
     addUser,
-    getUsers
+    getUsers,
+    getUser,
+    deleteUser
 }
