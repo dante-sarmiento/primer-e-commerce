49a34a3c9166d8c2a223a58c4b6dbf43d2fb5dab
dante-sarmiento <dante.sarmientof5@gmail.com>
2022-02-24 20:16:10 -0300

    agregando propiedades en el schema

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -1,10 +1,23 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var rolesValidos = [
+    'ADMIN_ROLE',
+    'USER_ROLE', //personal interno de la empresa
+    'CLIENT_ROLE'//clientes usuarios de mi app
+]
+
 var UserSchema = new Schema({
-    fullName: { type: String },
-    email: { type: String },
-    password: { type: String }
+    fullName: { type: String, required: true, maxlength: 40 },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, maxlength: 14 },
+    password: { type: String },
+    adress: {
+        street: { type: String },
+        steet_numer: { type: Number },
+    },
+    active: { type: Boolean, default: false },
+    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos }
 });
 
 module.exports = mongoose.model('User', UserSchema)
